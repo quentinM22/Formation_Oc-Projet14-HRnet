62984cd03c84075d01cc5a05ecc6bf93dca6982d
quentinM22 <quentin.meunier1@laposte.net>
2023-08-23 17:44:10 +0200

    feat: refresh form

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react"
 import "./Form.css"
 
-import states from "../../data/state.json"
-import department from "../../data/department.json"
+import statesData from "../../data/state.json"
+import departmentData from "../../data/department.json"
 import { DatePicker } from "@quentinm22/datepicker-react-cmp"
 import { Modal } from "@quentinm22/modal-cmp-react"
 import Select from "react-select"
@@ -12,9 +12,18 @@ import { addEmployee } from "../../RTK/Slice/dataSlice"
 const Form = () => {
 	const dispatch = useDispatch()
 
+
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [startDate, setStartDate] = useState("")
+	const [dateOfBirth, setdateOfBirth] = useState("");
+	const [street, setStreet] = useState("");
+	const [city, setCity] = useState("");
+	const [state, setState] = useState("")
+	const [zipCode, setZipCode] = useState("");
+	const [department, setDepartment] = useState("");
+
 	const [maxBirthdate, setMaxBirthdate] = useState("")
-	const [selectedOptionState, setSelectedOptionState] = useState("")
-	const [selectedOptionDep, setSelectedOptionDep] = useState("")
 	const [toggle, setToggle] = useState(false)
 	const [error, setError] = useState(false)
 	const [errors, setErrors] = useState({
@@ -57,15 +66,15 @@ const Form = () => {
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		const formData = {
-			 firstName: document.querySelector("#first-name").value,
-			 lastName: document.querySelector("#last-name").value,
-			 startDate: document.querySelector("#start-date").value,
-			 department: selectedOptionDep ? selectedOptionDep.value : "",
-			 dateOfBirth: document.querySelector("#date-of-birth").value,
-			 street: document.querySelector("#street").value,
-			 city: document.querySelector("#city").value,
-			 state: selectedOptionState ? selectedOptionState.value : "",
-			 zipCode: document.querySelector("#zip-code").value,
+			 firstName: firstName,
+			 lastName: lastName,
+			 startDate: startDate,
+			 department: department ? department.value : "",
+			 dateOfBirth: dateOfBirth,
+			 street: street,
+			 city: city,
+			 state: state ? state.value : "",
+			 zipCode: zipCode,
 		}
 
         const formErrors = {
@@ -79,14 +88,37 @@ const Form = () => {
 			state: !formData.state ? "State of birth is required" : "",
 			zipCode: formData.zipCode === "" ? "Zip code is required" : "",
 		}
+
+		const refresh = () => {
+			setErrors({
+				firstName: "",
+				lastName: "",
+				startDate: "",
+				department: "",
+				dateOfBirth: "",
+				street: "",
+				city: "",
+				state: "",
+				zipCode: "",
+			  });
+			setFirstName("")
+			setLastName("")
+			setCity("")
+			setStreet("")
+			setZipCode("")
+			setStartDate("")
+			setdateOfBirth("")
+			setDepartment("")
+			setState("")
+		}
+
 		const isFormValid = validateForm(formData, formErrors)
 
 		if (isFormValid) {
 		  setError(false)
 		  setToggle(true)
 		  dispatch(addEmployee(formData))
-		  const form = document.querySelector("form")
-		  form.reset()
+			refresh()
 		} else {
 		  setError(true)
 		  setToggle(true)
@@ -136,25 +168,25 @@ const Form = () => {
 					<div className="first-section">
 						<div>
 							<label htmlFor="first-name">First Name</label>
-							{errors.firstName ? (<input type="text" id="first-name" className="error-input"/>):(<input type="text" id="first-name"/>)}
+							{errors.firstName ? (<input type="text" id="first-name" className="error-input" onChange={(e) => setFirstName(e.target.value)} value={firstName}/> ):(<input type="text" id="first-name" onChange={(e) => setFirstName(e.target.value)} value={firstName} />)}
 							{errors.firstName && <small className="error-message">{errors.firstName}</small>}
 						</div>
 						<div>
 							<label htmlFor="last-name">Last Name</label>
-							{errors.lastName ? (<input type="text" id="last-name" className="error-input"/>):(<input type="text" id="last-name"/>)}
+							{errors.lastName ? (<input type="text" id="last-name" className="error-input" onChange={(e) => setLastName(e.target.value)} value={lastName}/>):(<input type="text" id="last-name" onChange={(e) => setLastName(e.target.value)} value={lastName}/>)}
 							{errors.lastName && <small className="error-message">{errors.lastName}</small>}
 						</div>
 
 						<div>
 							<label htmlFor="date-of-birth">Date of Birth</label>
-							{errors.startDate ? (<DatePicker idInput="date-of-birth" maxDate={maxBirthdate} className="error-input" />):(<DatePicker idInput="date-of-birth" maxDate={maxBirthdate} />)}
+							{errors.startDate ? (<DatePicker idInput="date-of-birth" maxDate={maxBirthdate} className="error-input" setState={setdateOfBirth} state={dateOfBirth}/>):(<DatePicker idInput="date-of-birth" maxDate={maxBirthdate} setState={setdateOfBirth}  state={dateOfBirth}/>)}
 							{errors.dateOfBirth && <small className="error-message">{errors.dateOfBirth}</small>}
 
 						</div>
 
 						<div>
 							<label htmlFor="start-date">Start Date</label>
-							{errors.startDate ? (<DatePicker idInput="start-date" className="error-input" />):(<DatePicker idInput="start-date" />)}
+							{errors.startDate ? (<DatePicker idInput="start-date" className="error-input" setState={setStartDate} state={startDate}/>):(<DatePicker idInput="start-date" setState={setStartDate} state={startDate}/>)}
 							{errors.startDate && <small className="error-message">{errors.startDate}</small>}
 						</div>
 					</div>
@@ -163,28 +195,27 @@ const Form = () => {
 						<legend>Address</legend>
 						<div>
 							<label htmlFor="street">Street</label>
-							{errors.street ? (<input type="text" id="street" className="error-input"/>):(<input type="text" id="street"/>)}
-
+							{errors.street ? (<input type="text" id="street" className="error-input" onChange={(e) => setStreet(e.target.value)} value={street}/>):(<input type="text" id="street" onChange={(e) => setStreet(e.target.value)} value={street}/>)}
 							{errors.street && <small className="error-message">{errors.street}</small>}
 
 						</div>
 
 						<div>
 							<label htmlFor="city">City</label>
-							{errors.city ? (<input type="text" id="city" className="error-input"/>):(<input type="text" id="city"/>)}
-
+							{errors.city ? (<input type="text" id="city" className="error-input" onChange={(e) => setCity(e.target.value)} value={city}/>):(<input type="text" id="city" onChange={(e) => setCity(e.target.value)} value={city}/>)}
 							{errors.city && <small className="error-message">{errors.city}</small>}
 
 						</div>
 						<div>
 							<label htmlFor="state">State</label>
-							<Select onChange={setSelectedOptionState} options={states} 
+							<Select onChange={setState} options={statesData} 
 							styles={{
 								control: (provided) => ({
 								  ...provided,
 								  border: errors.state ? "1px solid red" : "1px solid #ced4da", // Changez les couleurs selon vos besoins
 								}),
 							  }}
+							  value={state}
 							/>
 							{errors.state && <small className="error-message">{errors.state}</small>}
 
@@ -192,19 +223,20 @@ const Form = () => {
 
 						<div>
 							<label htmlFor="zip-code">Zip Code</label>
-							{errors.zipCode ? (<input type="text" id="zip-code" className="error-input"/>):(<input type="text" id="zip-code"/>)}
+							{errors.zipCode ? (<input type="text" id="zip-code" className="error-input" onChange={(e) => setZipCode(e.target.value)} value={zipCode}/>):(<input type="text" id="zip-code" onChange={(e) => setZipCode(e.target.value)} value={zipCode}/>)}
 							{errors.zipCode && <small className="error-message">{errors.zipCode}</small>}
 						</div>
 					</fieldset>
 					<div className="second-section">
 						<label htmlFor="department">Department</label>
-						<Select onChange={setSelectedOptionDep} options={department}
+						<Select onChange={setDepartment} options={departmentData}
 							styles={{
 								control: (provided, state) => ({
 								  ...provided,
 								  border: errors.department ? "1px solid red" : "1px solid #ced4da", // Changez les couleurs selon vos besoins
 								}),
 							  }}
+							  value={department}
 						 />
 						{errors.department && <small className="error-message">{errors.department}</small>}
 					</div>
